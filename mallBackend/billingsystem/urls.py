6d073a262b552c62292802d0e90e84513111753a
Mahesh <maheshgugulot@gmail.com>
2024-04-07 08:57:54 +0530

    updates in database sections in settings.py

--- mallBackend/billingsystem/urls.py
+++ mallBackend/billingsystem/urls.py
@@ -16,8 +16,6 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     path('signuppage/', login_view, name='signuppage'),
     path('logout/', logout_view, name='logout'),
-
-    path('api/token/', obtain_auth_token, name='token_obtain_pair'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
